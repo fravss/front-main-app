2278e556f779c5c50ca98a651c7f087a5d2504e8
Ana Flavia <amartins11@uolinc.com>
2025-11-13 10:29:59 -0300

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080/api' 
+    apiUrl: 'http://18.234.187.101:8080/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'http://18.234.187.101:8080/api'
 };
